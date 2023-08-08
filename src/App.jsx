502b8a7bd5f5751632221cd06a4d5878f6c1efa9
Braxton <65457602+braxttsl-dev@users.noreply.github.com>
2023-08-07 23:38:09 -0400

    initital commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from "./assets/logo.svg"
+import sun from "./assets/icon-sun.svg"
+import moon from "./assets/icon-moon.svg"
+import profile from "./assets/image-avatar.jpg"
+function App() {
+
+  return (
+    <>
+    <main className="light">
+
+     
+
+    </main>
+    </>
+  )
+}
+
+export default App
