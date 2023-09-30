6f013a4d6d104dc1b6e6b78d4ccd5aba62712728
Braxton <58635532+Braxton-Jones@users.noreply.github.com>
2023-09-30 10:09:31 -0400

    api fix

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://invoice-mgqv.onrender.com/',
 });
 
 export const fetchInvoices = async () => {
